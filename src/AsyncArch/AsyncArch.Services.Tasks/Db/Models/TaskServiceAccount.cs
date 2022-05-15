66ab7b8d4d67251b9e4cb18ad861b876b45cadda
IlyaMurashov <murashov.ilya@gmail.com>
2022-05-15 14:15:47 +0200

    Change DB context to use default Id as PK

--- src/AsyncArch/AsyncArch.Services.Tasks/Db/Models/TaskServiceAccount.cs
+++ src/AsyncArch/AsyncArch.Services.Tasks/Db/Models/TaskServiceAccount.cs
@@ -4,7 +4,7 @@ namespace AsyncArch.Services.Tasks.Db.Models;
 
 public class TaskServiceAccount
 {
-    [Key]
+    public int Id { get; set; }
     public Guid UserId { get; set; }
     public string? UserName { get; set; }
     public string? Role { get; set; }
--- src/AsyncArch/AsyncArch.Services.Tasks/Db/Models/TaskServiceTask.cs
+++ src/AsyncArch/AsyncArch.Services.Tasks/Db/Models/TaskServiceTask.cs
@@ -6,13 +6,14 @@ namespace AsyncArch.Services.Tasks.Db.Models;
 [Index(nameof(Assignee), IsUnique = false)]
 public class TaskServiceTask
 {
-    [Key]
+    public int Id { get; set; }
+    
     public Guid Uuid { get; set; }
     
     public Guid Assignee { get; set; }
-    
+
     [Required]
-    public string Description { get; set; }
+    public string Description { get; set; } = null!;
     
     public bool IsDone { get; set; }
 }
